86dbc270b72052c9c2db7ec3ba51d359d0882e6e
Isaac Calligeros <isaac.calligeros@gmail.com>
2021-05-01 18:38:59 +0930

    Test controller for prod

---
+++ src/WebUI/Controllers/testController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HomiePages.Application.ServiceInterfaces;
+using HomiePages.Domain.Entities;
+using HomiePages.WebUI.Models.APIModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace HomiePages.WebUI.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    public class TestController : ApiControllerBase
+    {
+        public TestController()
+        {
+        }
+
+        [HttpGet]
+        public string FindOrCreate(long containerId)
+        {
+            return "This is from the test controller";
+        }
+    }
+}
