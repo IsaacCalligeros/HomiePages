a74bde73933bedc9e6caa319179a29d5374840b3
Isaac Calligeros <isaac.calligeros@gmail.com>
2021-05-21 22:53:01 +0930

    logs

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using HomiePages.Infrastructure.Identity;
 using HomiePages.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
@@ -6,54 +8,46 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
 
-namespace HomiePages.WebUI
-{
-    public class Program
-    {
-        public async static Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
+namespace HomiePages.WebUI {
+    public class Program {
+        public async static Task Main (string[] args) {
+            var host = CreateHostBuilder (args)
+                .ConfigureLogging((hostingContext, logging) => {
+                    logging.AddFile("logs/homiepagesApi-{Date}.txt");
+                }).Build();
 
-            using (var scope = host.Services.CreateScope())
-            {
+            using (var scope = host.Services.CreateScope ()) {
                 var services = scope.ServiceProvider;
 
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
+                try {
+                    var context = services.GetRequiredService<ApplicationDbContext> ();
 
-                    if (context.Database.IsSqlServer())
-                    {
-                        context.Database.Migrate();
-                    }                   
+                    if (context.Database.IsSqlServer ()) {
+                        context.Database.Migrate ();
+                    }
 
-                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>> ();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>> ();
 
-                    await ApplicationDbContextSeed.SeedDefaultUserAsync(userManager, roleManager);
-                    await ApplicationDbContextSeed.SeedSampleDataAsync(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    await ApplicationDbContextSeed.SeedDefaultUserAsync (userManager, roleManager);
+                    await ApplicationDbContextSeed.SeedSampleDataAsync (context);
+                } catch (Exception ex) {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>> ();
 
-                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+                    logger.LogError (ex, "An error occurred while migrating or seeding the database.");
 
                     throw;
                 }
             }
 
-            await host.RunAsync();
+            await host.RunAsync ();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder (string[] args) =>
+            Host.CreateDefaultBuilder (args)
+            .ConfigureWebHostDefaults (webBuilder => {
+                webBuilder.UseStartup<Startup> ();
+            });
     }
 }
